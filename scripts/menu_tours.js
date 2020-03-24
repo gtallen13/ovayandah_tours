3228627621eeb39bbae938f7660aa23a87e38904
Thomas Allen <gtallenpadi13@gmail.com>
2020-03-24 16:39:04 -0600

    Cambio de tours finalizado. Se agregaron imagenes y se cambiaron nombres en la carpete /img.

--- scripts/menu_tours.js
+++ scripts/menu_tours.js
@@ -11,20 +11,54 @@ conexion.connect(function(err)
     if (err) throw err;
     console.log ('Conexion Exitosa');
 })
-const toursinfo = document.getElementById('tours_info');
-const toursmenu = document.getElementById('tours_menu');
-let html = "<ul>"
-let sql = 'select nombre from tours'
+const toursinfo = document.getElementById('tours_info'); //div donde se mostrara la informacion del tour
+const toursmenu = document.getElementById('tours_menu'); //div donde se mostrara el menu de tours
+const tours_imagen = document.getElementById('caja'); //div donde se mostrara la imagen
+let html_menu = "<ul>" //para desplegar menu de tours
+let html_info = ''; //para desplegar titulo del tour e informacion del tour
+
+let sql = 'select nombre, precio, descripcion from tours' 
+let i = 1;
+
+//consulta para mostrar el primer tour
 conexion.query (sql, 
     function(err, filas, fields){
         if (err) throw err;
-
+        tours_imagen.style.backgroundImage = 'url("./carpeta.css/img/tour1.jpg")';
+        html_info += `<h2>${filas[0].nombre}</h2>`
+        html_info += `<p>$ ${filas[0].precio}</p>`
+        html_info += `<p>${filas[0].descripcion}</p>`
+        
         for (let fila of filas)
         {
-            html += `<a href = "#">${fila.nombre}</a>`;
+            html_menu += `<button class = "tablinks" onclick = "cambioTours('click', ${i})"  >${fila.nombre}</a>`;
+            i++;
         }
-        html += '</ul>';
-        toursmenu.innerHTML = html;
+        html_menu += '</ul>';
+        toursmenu.innerHTML = html_menu;
+        toursinfo.innerHTML = html_info;
     });
 
+//utilizada para navegar entre los tours disponibles
+function cambioTours(evt, id_tour)
+{
+    // var j, tablink;
+
+    // tablinks = document.getElementsByClassName('tablinks');
+    // for (j = 0; i <tablinks.length; j++)
+    // {
+    //     tablinks[i].className = tablinks[i].className.replace(" active", ""); 
+    // }
+    html_info = ''
+    let sql = `select * from tours`
+    conexion.query (sql, function(err, resultados, campo)
+    {
+        html_info += `<h2>${resultados[id_tour - 1].nombre}</h2>`
+        html_info += `<p>$ ${resultados[id_tour - 1].precio}</p>`
+        html_info += `<p>${resultados[id_tour - 1].descripcion}</p>`
+        toursinfo.innerHTML = html_info;
+        console.log(`url("./carpeta.css/img/tour${id_tour - 1}.jpg")`)
+        tours_imagen.style.backgroundImage = `url("./carpeta.css/img/tour${id_tour}.jpg")`;
+    });
+}
 
