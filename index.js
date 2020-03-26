d9a5922de292c4fe7ed663aa842f9edf8a3d4886
Thomas Allen <gtallenpadi13@gmail.com>
2020-03-26 17:07:03 -0600

    Pagina Inicio: Cambiando fondo y Cambiando titulos en las imagenes

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 1425,
     height: 785,
-    minWidth: 393,
+    minWidth: 700,
+    minHeight: 500,
     center: true,
     webPreferences: {
       nodeIntegration: true
