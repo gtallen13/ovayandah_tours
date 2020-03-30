69a0c4c872ecb256ad6af4598f55b037495104fc
Thomas Allen <gtallenpadi13@gmail.com>
2020-03-30 14:03:03 -0600

    Finalizando los tres graficos

--- scripts/graficos.js
+++ scripts/graficos.js
@@ -1,5 +1,7 @@
 const Chart = require('chart.js');
-let myChart = document.getElementById('chart').getContext('2d');
+let chartUbicaciones = document.getElementById('chart_ubicaciones').getContext('2d');
+let chartTours = document.getElementById('chart_tours').getContext('2d');
+let chartActividaes = document.getElementById('chart_actividades').getContext('2d');
 let arrUbicaciones = []
 let arrCantReservaciones_ubicaciones = []
 let arrTours =  []
@@ -49,180 +51,174 @@ order by count(r.tours_id) desc
 limit 5;`
 
 //Mostrara un grafico con las top 5 ubicaciones
-function ubicacionesTop()
-{       
-        
-        conexion.query(sql_ubcacionesTop, function(err, resultados, fields)
+       
+conexion.query(sql_ubcacionesTop, function(err, resultados, fields)
+{
+    for (let resultado of resultados)
     {
-        for (let resultado of resultados)
+        arrUbicaciones.push(resultado.Ubicacion); //llenando el arreglo de las ubicaciones
+        arrCantReservaciones_ubicaciones.push(resultado.CantidadReservaciones) //llenando el arreglo de las Cantidades Reservadas
+    }
+    //haciendo el grafico
+    let barChart_ubicaciones = new Chart(chartUbicaciones, {
+        type: 'bar',
+        data: 
         {
-            arrUbicaciones.push(resultado.Ubicacion); //llenando el arreglo de las ubicaciones
-            arrCantReservaciones_ubicaciones.push(resultado.CantidadReservaciones) //llenando el arreglo de las Cantidades Reservadas
-        }
-        //haciendo el grafico
-        let barChart_ubicaciones = new Chart(myChart, {
-            type: 'bar',
-            data: 
+            labels: arrUbicaciones,
+            datasets: 
+            [{
+                label: '# de Reservaciones',
+                data: arrCantReservaciones_ubicaciones,
+                //to apply different colors to columns just send to color in an array
+                backgroundColor: 'blue',
+                hoverBorderWidth: 3,
+                hoverBorderColor: '#000' 
+            }]
+        },
+        options: 
+        {
+            title:
             {
-                labels: arrUbicaciones,
-                datasets: 
-                [{
-                    label: '# de Reservaciones',
-                    data: arrCantReservaciones_ubicaciones,
-                    //to apply different colors to columns just send to color in an array
-                    backgroundColor: 'blue',
-                    hoverBorderWidth: 3,
-                    hoverBorderColor: '#000' 
-                }]
+                display: true,
+                text: 'Ubicaciones mas Reservadas',
+                fontSize:25,
+                fontFamily: 'Georgia'
             },
-            options: 
+            legend:
+            {  
+                display: true,
+                position: 'right',
+                labels:
+                {
+                    fontColor:'#000'
+                }   
+            },
+            layout:
             {
-                title:
+                padding:
                 {
-                    display: true,
-                    text: 'Ubicaciones mas Reservadas',
-                    fontSize:25,
-                    fontFamily: 'Georgia'
-                },
-                legend:
-                {  
-                    display: true,
-                    position: 'right',
-                    labels:
-                    {
-                        fontColor:'#000'
-                    }   
-                },
-                layout:
-                {
-                    padding:
-                    {
-                        left: 50,
-                        right: 0,
-                        bottom: 0,
-                        top:0
-                    }
+                    left: 50,
+                    right: 0,
+                    bottom: 0,
+                    top:0
                 }
             }
-        });
+        }
     });
-}
+});
+
 
 //Mostrara los tours mas populares
-function toursTop()
+conexion.query(sql_toursTop, function(err, resultados, campos)
 {
-    conexion.query(sql_toursTop, function(err, resultados, campos)
+    for (let resultado of resultados)
     {
-        for (let resultado of resultados)
+        arrTours.push(resultado.NombreTour)
+        arrCantReservaciones_tours.push(resultado.CantidadReservaciones)
+    }
+        let barChart_tours = new Chart(chartTours, {
+        type: 'bar',
+        data: 
         {
-            arrTours.push(resultado.NombreTour)
-            arrCantReservaciones_tours.push(resultado.CantidadReservaciones)
-        }
-            let barChart_tours = new Chart(myChart, {
-            type: 'bar',
-            data: 
+            labels: arrTours,
+            datasets: 
+            [{
+                label: '# de Reservaciones',
+                data: arrCantReservaciones_tours,
+                //to apply different colors to columns just send to color in an array
+                backgroundColor: 'blue',
+                hoverBorderWidth: 3,
+                hoverBorderColor: '#000' 
+            }]
+        },
+        options: 
+        {
+            title:
             {
-                labels: arrTours,
-                datasets: 
-                [{
-                    label: '# de Reservaciones',
-                    data: arrCantReservaciones_tours,
-                    //to apply different colors to columns just send to color in an array
-                    backgroundColor: 'blue',
-                    hoverBorderWidth: 3,
-                    hoverBorderColor: '#000' 
-                }]
+                display: true,
+                text: 'Tours mas Reservadas',
+                fontSize:25,
+                fontFamily: 'Georgia'
             },
-            options: 
+            legend:
+            {  
+                display: true,
+                position: 'right',
+                labels:
+                {
+                    fontColor:'#000'
+                }   
+            },
+            layout:
             {
-                title:
+                padding:
                 {
-                    display: true,
-                    text: 'Tours mas Reservadas',
-                    fontSize:25,
-                    fontFamily: 'Georgia'
-                },
-                legend:
-                {  
-                    display: true,
-                    position: 'right',
-                    labels:
-                    {
-                        fontColor:'#000'
-                    }   
-                },
-                layout:
-                {
-                    padding:
-                    {
-                        left: 50,
-                        right: 0,
-                        bottom: 0,
-                        top:0
-                    }
+                    left: 50,
+                    right: 0,
+                    bottom: 0,
+                    top:0
                 }
             }
-        });
-    })
-}
+        }
+    });
+})
+
 
 
 //Mostrara las actividades mas populares
-function actividadesTop()
+conexion.query(sql_actividadesTop, function(err, resultados, campos)
 {
-    conexion.query(sql_actividadesTop, function(err, resultados, campos)
+    for (let resultado of resultados)
     {
-        for (let resultado of resultados)
+        arrActividades.push(resultado.NombreActividad)
+        arrCantReservaciones_actividades.push(resultado.CantidadReservaciones)
+    }
+        let barChart_actividades = new Chart(chartActividaes, {
+        type: 'bar',
+        data: 
         {
-            arrActividades.push(resultado.NombreActividad)
-            arrCantReservaciones_actividades.push(resultado.CantidadReservaciones)
-        }
-            let barChart_actividades = new Chart(myChart, {
-            type: 'bar',
-            data: 
+            labels: arrActividades,
+            datasets: 
+            [{
+                label: '# de Reservaciones',
+                data: arrCantReservaciones_actividades,
+                //to apply different colors to columns just send to color in an array
+                backgroundColor: 'blue',
+                hoverBorderWidth: 3,
+                hoverBorderColor: '#000' 
+            }]
+        },
+        options: 
+        {
+            title:
             {
-                labels: arrActividades,
-                datasets: 
-                [{
-                    label: '# de Reservaciones',
-                    data: arrCantReservaciones_actividades,
-                    //to apply different colors to columns just send to color in an array
-                    backgroundColor: 'blue',
-                    hoverBorderWidth: 3,
-                    hoverBorderColor: '#000' 
-                }]
+                display: true,
+                text: 'Actividades mas Populares',
+                fontSize:25,
+                fontFamily: 'Georgia'
             },
-            options: 
+            legend:
+            {  
+                display: true,
+                position: 'right',
+                labels:
+                {
+                    fontColor:'#000'
+                }   
+            },
+            layout:
             {
-                title:
+                padding:
                 {
-                    display: true,
-                    text: 'Actividades mas Populares',
-                    fontSize:25,
-                    fontFamily: 'Georgia'
-                },
-                legend:
-                {  
-                    display: true,
-                    position: 'right',
-                    labels:
-                    {
-                        fontColor:'#000'
-                    }   
-                },
-                layout:
-                {
-                    padding:
-                    {
-                        left: 50,
-                        right: 0,
-                        bottom: 0,
-                        top:0
-                    }
+                    left: 50,
+                    right: 0,
+                    bottom: 0,
+                    top:0
                 }
             }
-        });
-    })
-}
+        }
+    });
+})
+
 
 
