426ae08c39aabeafa170dd81c3bbbcb998e05d48
girondarla@gmail.com <girondarla@gmail.com>
2020-04-01 23:10:08 -0600

    Modificacion del diseño de Login

--- scripts/menu_tours.js
+++ scripts/menu_tours.js
@@ -3,7 +3,7 @@ const conexion = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
     password: '',
-    database: 'toursdb'
+    database: 'ovayandah_tours'
 });
 
 conexion.connect(function(err)
